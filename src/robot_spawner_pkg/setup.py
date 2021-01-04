4f1b9bfc1954c927bcf482aa6241caf393eada69
ntraichel <nele.traichel@ovgu.de>
2021-01-04 09:32:45 +0100

    moved maps to this repo; tidy up the share dir in setup.py

--- src/robot_spawner_pkg/setup.py
+++ src/robot_spawner_pkg/setup.py
@@ -14,7 +14,9 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.launch.py')),
         (os.path.join('share', package_name), glob('params/*')),
-        (os.path.join('share', package_name), glob('rviz/*.rviz'))
+        (os.path.join('share', package_name), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
