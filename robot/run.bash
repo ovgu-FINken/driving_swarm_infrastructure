268b439880e338eeb52ce41fc5a3a8ffeec6a6f3
Joel Dierkes <joel.dierkes@ovgu.de>
2021-02-08 14:57:26 +0100

    Changed comment

--- robot/run.bash
+++ robot/run.bash
@@ -6,4 +6,6 @@ source /opt/ros/foxy/setup.bash
 source /home/turtle/driving_swarm_infrastructure/install/setup.bash
 source /home/turtle/turtlebot3/install/setup.bash
 export TURTLEBOT3_MODEL=burger
+
+# at least one foreground process needs to be running
 ros2 launch system_status robot_system_bringup.launch.py
