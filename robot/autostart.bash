ae4edc03a58ed62c2eaf0c356fd4f59762dfca00
Joel Dierkes <joel.dierkes@ovgu.de>
2021-02-08 14:05:27 +0100

    Fix error when auto updating robot

--- robot/autostart.bash
+++ robot/autostart.bash
@@ -1,15 +1,23 @@
 #!/usr/bin/bash
+# Checks for updates on the driving_swarm_repo and starts ros nodes
+
+PROJECT_DIR="/home/turtle/driving_swarm_infrastructure"
+ROBOT_DIR="${PROJECT_DIR}/robot"
+ROBOT_BRANCH="robot-release"
+
 source ~/.bashrc
+
+cd "${PROJECT_DIR}"
 
 git fetch
 HEADHASH=$(git rev-parse HEAD)
-UPSTREAMHASH=$(git rev-parse robot-release@{upstream})
+UPSTREAMHASH=$(git rev-parse "${ROBOT_BRANCH}@{upstream}")
 
 if [ "$HEADHASH" != "$UPSTREAMHASH"  ]
 then
     # new changes
-    git pull
-    sh /home/turtle/driving_swarm_infrastructure/robot/install.sh
+    git pull --force
+    bash "${ROBOT_DIR}/install.bash"
 fi
 
-sh /home/turtle/driving_swarm_infrastructure/robot/run.sh
+bash "${ROBOT_DIR}/run.bash"
