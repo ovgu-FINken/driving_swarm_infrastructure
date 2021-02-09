51d7098c1dffa3bee69a4839b8f506298efc1afe
Sebastian Mai <sebastian.mai@ovgu.de>
2021-02-09 14:51:11 +0100

    adding package for supervision of multi robot expirements
    * start behaviour by defined start service with empty type
    * planned: data exfiltration
    * planned: launch files for specific experiments/papers

---
+++ src/experiment_supervisor/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'experiment_supervisor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Sebastian Mai',
+    maintainer_email='sebastian.mai@ovgu.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'fixed_trajectory_supervisor = experiment_supervisor.fixed_trajectory_supervisor:main'
+        ],
+    },
+)
