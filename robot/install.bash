8f2bbf489adc63f014177decbd2722f982b0b7d9
Joel Dierkes <joel.dierkes@ovgu.de>
2021-02-11 14:09:05 +0100

    Change the installation at robot startup

--- robot/install.bash
+++ robot/install.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 # Executes after driving_swarm_infrastructure_update
 
+cd $HOME/drving_swarm_infrastructure
+
+sudo rm -r build/ install/ log/
+
+CC=gcc CXX=g++ colcon build --symlink-install --cmake-clean-cache --parallel-workers 1
 sudo reboot
