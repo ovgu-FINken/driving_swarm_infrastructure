1fe0d0e31f0f2a100ade5cce069f5fb0dad067a7
Joel Dierkes <joel.dierkes@ovgu.de>
2021-03-04 19:02:21 +0000

    Add a CLI for the experiment Measurement package

--- src/experiment_measurement/experiment_measurement/data_aggregation.py
+++ src/experiment_measurement/experiment_measurement/data_aggregation.py
@@ -1,4 +1,7 @@
 """Aggregate the data to one table."""
+import argparse
+import os
+
 import math
 
 import pandas as pd
@@ -8,6 +11,7 @@ from transformations import euler_from_quaternion
 from rosbag2df import read_rosbag_all_in_one
 import data_aggregation_basti
 import data_aggregation_helper
+
 
 def aggregate_tables(df, table_column_config, step_size):
     """Create a table for each robot with corresponding values."""
@@ -33,7 +37,11 @@ def aggregate_tables(df, table_column_config, step_size):
             ]
 
             for t in range(xmin, xmax, step_size):
-                conf = data_aggregation_helper.TableConfig(robot_df_topic, t, t - step_size)
+                conf = data_aggregation_helper.TableConfig(
+                    robot_df_topic,
+                    t,
+                    t - step_size
+                )
                 try:
                     topic_data = topic(conf)
                 except Exception as e:
@@ -46,16 +54,58 @@ def aggregate_tables(df, table_column_config, step_size):
 
 
 def main():
-    """Is the main entry point when called from command."""
-    db_path = '/home/turtle-student/Downloads/\
-Telegram Desktop/rosbag2_2021_02_09-15_11_58_0.db3'
-    data_dict = read_rosbag_all_in_one(db_path)
-    print(aggregate_tables(
+    """This is the main entry point when called from command."""
+    my_parser = argparse.ArgumentParser(
+        description = 'Aggregates the turtlebot data to one table'
+    )
+
+    my_parser.add_argument('input_file',
+                           type=str,
+                           help='the path to the db3 database file')
+    my_parser.add_argument('--force',
+                           action='store_true',
+                           help='Don\'t ask for approval if a file would be overwritten.')
+    my_parser.add_argument('--step_size',
+                           type=int,
+                           help='the step size in nanoseconds, default in 1 second',
+                           default=10**9)
+    my_parser.add_argument('--of',
+                           metavar='output_folder',
+                           type=str,
+                           help='the path to the export csv file, optional')
+
+    args = my_parser.parse_args()
+
+    input_file_path      = args.input_file
+    force                = args.force
+    try:
+        step_size        = int(args.step_size)
+    except ValueError:
+        raise ValueError("Step Size could not be converted to int!")
+    output_folder_path   = args.of
+
+    assert os.path.isfile(input_file_path),\
+        'The input file path specified does not exist'
+
+
+    if output_folder_path and not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+
+    data_dict = read_rosbag_all_in_one(input_file_path)
+    data = aggregate_tables(
         data_dict['rosbag'],
         data_aggregation_basti.table_column_config,
-        10**9
-    )['robot1'])
-
+        step_size
+    )
+    for robot, r_data in data.items():
+        if not output_folder_path:
+            print(robot + ":")
+            print(r_data)
+        else:
+            csv_output_path = os.path.join(output_folder_path, robot + ".csv")
+            if os.path.exists(csv_output_path) and not force:
+                raise Exception("File {} exists!".format(csv_output_path))
+            r_data.to_csv(csv_output_path)
 
 if __name__ == '__main__':
     main()
