242ae3403a4da300260d737c8a47f08b23366c7b
Sebastian Mai <sebastian.mai@ovgu.de>
2021-07-14 16:00:21 +0200

    adding Space Time A*

--- src/driving_swarm_nav_graph/driving_swarm_nav_graph/utils.py
+++ src/driving_swarm_nav_graph/driving_swarm_nav_graph/utils.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from dataclasses import dataclass
 from graph_tool.all import * #noqa
 from skimage import io, measure
+from deprecation import deprecated
 import yaml
 import os
 
@@ -269,6 +270,110 @@ class NavVisitor(AStarVisitor):
             raise StopSearch()
 
 
+class SpaceTimeVisitor(AStarVisitor):
+    def __init__(self, generating_graph, goal, limit=100, node_constraints=None, edge_constraints=None):
+        self.timeless = generating_graph
+        self.g = graph_tool.Graph(directed=True)
+        self.g.vp['index'] = self.g.new_vertex_property('int')
+        self.g.vp['t'] = self.g.new_vertex_property('int')
+        self.g.ep['dist'] = self.g.new_edge_property('double')
+        self.g.vp['cost'] = self.g.new_vertex_property('double')
+        self.g.vp['dist'] = self.g.new_vertex_property('double')
+
+        self.target = goal
+        for v in self.timeless.vertices():
+            n = self.g.add_vertex()
+            self.g.vp['index'][n] = int(v)
+            self.g.vp['t'][n] = 0
+        
+        self.touched_e = self.g.new_edge_property('bool')
+        self.touched_v = self.g.new_vertex_property('bool')
+        self.limit=limit
+        self.nc = node_constraints if node_constraints else {}
+        self.ec = edge_constraints if edge_constraints else {}
+
+    def get_node(self, t, index):
+        for v in self.g.vertices():
+            if self.g.vp['index'][v] == index and self.g.vp['t'][v] == t:
+                return v
+        return None
+
+    def add_vertex_if_not_exists(self, t, index):
+        if t in self.nc:
+            if index in self.nc[t]:
+                return None
+        n = self.get_node(t, index)
+        if n is not None:
+            return n
+        n = self.g.add_vertex()
+        self.g.vp['index'][n] = index
+        self.g.vp['t'][n] = t
+        self.g.vp['cost'][n] = np.inf
+        self.g.vp['dist'][n] = np.inf
+        return n
+
+    def add_edge_if_feasible(self, t, v1, v2):
+        """add an edge to the graph from v1 to v2 iff. the edge is not forbidden"""
+
+        i1 = self.g.vp['index'][v1]
+        i2 = self.g.vp['index'][v2]
+
+        if i1 == i2:
+            e = self.g.add_edge(v, vout)
+            self.g.ep['dist'][e] = .1
+            return e
+
+        if t in self.ec:
+            if (i1, i2) in self.ec[t]:
+                return None
+            if (i2, i1) in self.ec[t]:
+                return None
+        
+        e = self.g.add_edge(v1, v2)
+        self.g.ep['dist'][e] = self.timeless.ep['dist'][self.timeless.edge(i1, i2)]
+
+        return e
+
+
+    def add_outgoing_edges(self, v):
+        index = self.g.vp['index'][v]
+        t = self.g.vp['t'][v]
+        for n in self.timeless.vertex(index).all_neighbors():
+            vout = self.add_vertex_if_not_exists(t+1, int(n))
+            if vout is not None:
+                self.add_edge_if_feasible(t, v, vout)
+        # add edge to self node (waiting action)
+        
+        vout = self.add_vertex_if_not_exists(t+1, index)
+        if vout is not None:
+            e = self.g.add_edge(v, vout)
+            self.g.ep['dist'][e] = .1
+
+    def discover_vertex(self, u):
+        self.touched_v[u] = True
+        if self.g.vp['t'][u] > self.limit:
+            self.timed_target_node = None
+            raise StopSearch()
+        # add nodes and edges going out of this node
+        self.add_outgoing_edges(u)
+
+    def examine_edge(self, e):
+        self.touched_e[e] = True
+    
+    def edge_relaxed(self, e):
+        if self.g.vp['index'][e.target()] == int(self.target):
+            self.timed_target_node = e.target()
+            raise StopSearch()
+
+
+def check_node_constraints(g, v, nc):
+    t = g.vp['t'][v]
+    i = g.vp['index'][v]
+    if t not in nc:
+        return False
+    return i in nc[t]
+
+
 def pred_to_list(g, pred, start, goal):
     p = goal
     l = [p]
@@ -281,13 +386,66 @@ def pred_to_list(g, pred, start, goal):
     l.reverse()
     return l
 
-def path_poly(g, start, goal, eps=0.1):
-    g.set_edge_filter(None)
-    g.set_vertex_filter(None)
-    g.set_vertex_filter(g.vp['traversable'])
-    g.set_edge_filter(g.ep['traversable'])
-    sn = find_nearest_node(g, start)
-    gn = find_nearest_node(g, goal)
+def add_edge_constraints(edge_constraints, path):
+    """append the constraints resulting from particular path to the list of existing constraints"""
+    ec = {t: [e] for t, e in enumerate(zip(path[:-1], path[1:]))}
+    if edge_constraints is None:
+        return ec
+    for k, v in ec.items():
+        if k in edge_constraints:
+            edge_constraints[k] += v
+        else:
+            edge_constraints[k] = v
+        
+    return edge_constraints
+
+def add_node_constraints(node_constraints, path):
+    """append the constraints resulting from particular path to the list of existing constraints"""
+    ec = {t: [n] for t, n in enumerate(path)}
+    if node_constraints is None:
+        return ec
+    for k, v in ec.items():
+        if k in node_constraints:
+            node_constraints[k] += v
+        else:
+            node_constraints[k] = v
+        
+    return node_constraints
+
+def prioritized_plans(g, start_goal, edge_constraints=None, node_constraints=None, limit=10):
+    """compute a set of paths for multiple agents in the same graph.
+    first agent is planned first, constraints are created for the remaining agents
+    start_goal -- [(start, goal) for agent in agents]"""
+    # plan first path with A-Star
+    paths = []
+    for sn, gn in start_goal:
+        paths.append(find_constrained_path(g, sn, gn, edge_constraints=edge_constraints, node_constraints=node_constraints, limit=limit))
+        edge_constraints = add_edge_constraints(edge_constraints, paths[-1])
+        node_constraints = add_node_constraints(node_constraints, paths[-1])
+    
+    return paths
+
+def find_constrained_path(g, sn, gn, edge_constraints=None, node_constraints=None, limit=10):
+    if edge_constraints is None and node_constraints is None:
+        return find_path_astar(g, sn, gn)
+
+    nv = SpaceTimeVisitor(g, gn, node_constraints=node_constraints, edge_constraints=edge_constraints, limit=limit)
+    _, pred = astar_search(nv.g,
+        sn,
+        nv.g.ep['dist'],
+        nv,
+        implicit=True,
+        heuristic=lambda v: np.linalg.norm(np.array(g.vp['center'][nv.g.vp['index'][v]]) - g.vp['center'][gn]),
+        cost_map=nv.g.vp['cost'],
+        dist_map=nv.g.vp['dist']
+    )
+
+    l = pred_to_list(nv.g, pred, sn, nv.timed_target_node)
+    return [nv.g.vp["index"][v] for v in l]
+
+
+def find_path_astar(g, sn, gn):
+    """find shortest path through graph g with a* algorithm"""
     visitor = NavVisitor(g.new_vertex_property("bool"), g.new_edge_property("bool"), gn)
     _, pred = astar_search(g,
         sn,
@@ -295,13 +453,30 @@ def path_poly(g, start, goal, eps=0.1):
         visitor,
         heuristic=lambda v: np.linalg.norm(np.array(g.vp['center'][v]) - np.array(g.vp['center'][gn]))
     )
+    return pred_to_list(g, pred, sn, gn)
 
-    l = pred_to_list(g, pred, sn, gn)
-    poly = [g.vp['geometry'][p].inner.buffer(eps) for p in l]
-    poly += [g.ep['geometry'][g.edge(*e)].borderPoly.buffer(eps) for e in zip(l[:-1], l[1:])]
-    poly = shapely.ops.unary_union(poly).buffer(-eps).simplify(eps)
-    
-    return l, poly
+
+def poly_from_path(g, path, eps=0.1):
+    poly = [g.vp['geometry'][p].inner.buffer(eps) for p in path]
+    poly += [g.ep['geometry'][g.edge(*e)].borderPoly.buffer(eps) for e in zip(path[:-1], path[1:])]
+    return shapely.ops.unary_union(poly).buffer(-eps).simplify(eps)
+
+
+def path_from_positions(g, start, goal):
+    g.set_edge_filter(None)
+    g.set_vertex_filter(None)
+    g.set_vertex_filter(g.vp['traversable'])
+    g.set_edge_filter(g.ep['traversable'])
+    sn = find_nearest_node(g, start)
+    gn = find_nearest_node(g, goal)
+    return find_path_astar(g, sn, gn)
+
+
+@deprecated(details="use path_from_positions(g, start, goal) and poly_from_path(g, path, eps=...) instead")
+def path_poly(g, start, goal, eps=0.1):
+    l = path_from_positions(g, start, goal)
+    return l, poly_from_path(g, l, eps=eps)
+
 
 def waypoints_through_poly(g, poly, start, goal, eps=0.01):
     """ compute a waypoints of a linear line segement path through a polygon
