da0013a57b839e7fe97fb3ee23b633e6518c2013
Sebastian Mai <sebastian.mai@ovgu.de>
2021-09-08 17:18:38 +0200

    adding map without obstacles

--- src/driving_swarm_nav_graph/launch/planner_sim.launch.py
+++ src/driving_swarm_nav_graph/launch/planner_sim.launch.py
@@ -31,7 +31,7 @@ def controller_spawning(context, *args, **kwargs):
         'graph_file': os.path.join(
            get_package_share_directory('driving_swarm_bringup'),
            'maps',
-           'icra2021_map.yaml'),
+           'icra2021_map_no_obstacle.yaml'),
         }],
        output='screen',
     ))
@@ -74,7 +74,7 @@ def controller_spawning(context, *args, **kwargs):
             'graph_file': os.path.join(
                get_package_share_directory('driving_swarm_bringup'),
                'maps',
-               'icra2021_map.yaml'),
+               'icra2021_map_no_obstacle.yaml'),
             }],
            output='screen',
         ))
@@ -101,7 +101,7 @@ def generate_launch_description():
     args = {
          'behaviour': 'false',
          'world': 'icra2021_world.world',
-         'map': os.path.join(get_package_share_directory('driving_swarm_bringup'), 'maps' ,'icra2021_map.yaml'),
+         'map': os.path.join(get_package_share_directory('driving_swarm_bringup'), 'maps' ,'icra2021_map_no_obstacle.yaml'),
          'robots_file': os.path.join(get_package_share_directory('driving_swarm_bringup'), 'params', 'icra2021_sim.yaml'),
          'rosbag_topics_file': os.path.join(get_package_share_directory('trajectory_follower'), 'params', 'rosbag_topics.yaml'),
          'qos_override_file': os.path.join(get_package_share_directory('experiment_measurement'), 'params', 'qos_override.yaml')
