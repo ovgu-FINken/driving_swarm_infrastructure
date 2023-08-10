a9d084a180487f5671a0ea6e03235eb8c6cb3478
Sebastian Mai <sebastian.mai@ovgu.de>
2023-08-10 11:12:25 +0200

    updating map creation with new stuff

--- src/map_creation_pkg/launch/occupancy_grid_namespaced.launch.py
+++ src/map_creation_pkg/launch/occupancy_grid_namespaced.launch.py
@@ -44,8 +44,8 @@ def generate_launch_description():
 
         Node(
             package='cartographer_ros',
-            executable='occupancy_grid_node',
-            name='occupancy_grid_node',
+            executable='cartographer_occupancy_grid_node',
+            name='cartographer_occupancy_grid_node',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time}],
             namespace=namespace,
