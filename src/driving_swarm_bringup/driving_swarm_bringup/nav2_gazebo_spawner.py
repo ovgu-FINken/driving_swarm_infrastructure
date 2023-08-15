63d7a016ee0734cde932be5828bcff97620660f2
Sebastian Mai <sebastian.mai@ovgu.de>
2023-08-15 15:03:51 +0200

    Revert "add random delay to robot spawner"
    This reverts commit f8440f91e00f5ca6fe30c725b1466a7856f0429f.

--- src/driving_swarm_bringup/driving_swarm_bringup/nav2_gazebo_spawner.py
+++ src/driving_swarm_bringup/driving_swarm_bringup/nav2_gazebo_spawner.py
@@ -27,8 +27,6 @@ from lifecycle_msgs.srv import GetState
 import time
 import tf_transformations
 from driving_swarm_utils.node import DrivingSwarmNode
-import random
-import time
 
 
 class Spawner(DrivingSwarmNode):
@@ -167,10 +165,6 @@ def main():
 
     args, _ = parser.parse_known_args()
 
-    #Generates the random value in the given range.
-    sleep_time = random.uniform(0, 5)
-
-    time.sleep(sleep_time)
     rclpy.init()
     node = Spawner(args)
     node.wait_for_localization()
