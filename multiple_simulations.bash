f45285bc1fd8dabce49c5d0522435d3e62b0f890
Sebastian Mai <sebastian.mai@ovgu.de>
2024-05-31 11:03:39 +0200

    multiple_simulations.bash

--- multiple_simulations.bash
+++ multiple_simulations.bash
@@ -6,7 +6,7 @@ N_RUNS=$1
 MODE="fake"
 #MODE="real"
 #MODE="sim"
-export RUN_TIMEOUT="600.0"
+export RUN_TIMEOUT="900.0"
 export INIT_TIMEOUT="300.0"
 export ROS_SIMULATOR="gzserver"
 BASE_DIR=~/data/experiment_$(date +%Y-%m-%d)
@@ -20,40 +20,33 @@ make_runs() {
 			echo "Running $COMMAND with N_ROBOTS=$N_ROBOTS"
 			# Run the command with the current N_ROBOTS value
 			ros2 launch ccr ccr_$MODE.launch.py use_rviz:=false use_rosbag:=true n_robots:=$N_ROBOTS waypoints_file:=$MAP
-			mv rosbag* $DATA_DIR
+			mv rosbag* $BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
 		done
 	done
 }
 
 export MAP="icra2024_waypoints.yaml"
 export MODE="fake"
-
+export RUN_TIMEOUT="300.0"
 export CCR_VERSION="global_planner_baseline"
 
 export CCR_PRIORITIES="index"
-export DATA_DIR=$BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
 make_runs
 
 export CCR_PRIORITIES="same"
-export DATA_DIR=$BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
 make_runs
 
-export MODE="sim"
-export CCR_PRIORITIES="index"
-export DATA_DIR=$BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
-make_runs
-
-export CCR_PRIORITIES="same"
-export DATA_DIR=$BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
-make_runs
-
-# CCR
 export CCR_VERSION="global_planner"
-
-export MODE="fake"
-export DATA_DIR=$BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
 make_runs
 
 export MODE="sim"
-export DATA_DIR=$BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
+export RUN_TIMEOUT="900.0"
+export CCR_VERSION="global_planner_baseline"
+export CCR_PRIORITIES="index"
+make_runs
+
+export CCR_PRIORITIES="same"
+make_runs
+
+export CCR_VERSION="global_planner"
 make_runs
