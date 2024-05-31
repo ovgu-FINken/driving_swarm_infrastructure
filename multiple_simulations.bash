5c486f476bad7ae0800337abda91ce92448a0978
Sebastian Mai <sebastian.mai@ovgu.de>
2024-05-31 11:56:37 +0200

    correct mistake for data dir

--- multiple_simulations.bash
+++ multiple_simulations.bash
@@ -14,13 +14,14 @@ MAX_ROBOTS=5
 
 
 make_runs() {
+	DATA_DIR=$BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
 	mkdir -p $DATA_DIR
 	for ((RUN = 1; RUN <= $N_RUNS; RUN++)); do
 		for ((N_ROBOTS = 1; N_ROBOTS <= MAX_ROBOTS; N_ROBOTS++)); do
 			echo "Running $COMMAND with N_ROBOTS=$N_ROBOTS"
 			# Run the command with the current N_ROBOTS value
 			ros2 launch ccr ccr_$MODE.launch.py use_rviz:=false use_rosbag:=true n_robots:=$N_ROBOTS waypoints_file:=$MAP
-			mv rosbag* $BASE_DIR/$MODE.$CCR_PRIORITIES.$CCR_VERSION.$MAP
+			mv rosbag* $DATA_DIR
 		done
 	done
 }
