ee6623c5ccf633b78d616aa6db1a899a90e7b6dc
Lasse Slaar <lasse.slaar@st.ovgu.de>
2025-08-27 14:12:37 +0200

    removed second launch description

--- src/msx/launch/msx.launch.py
+++ src/msx/launch/msx.launch.py
@@ -100,36 +100,3 @@ def controller_spawning(context, *args, **kwargs):
     
     return controllers
 
-
-def generate_launch_description():
-    args = {
-         'behaviour': 'false',
-         'world': 'icra2024.world',
-         'map': os.path.join(get_package_share_directory('driving_swarm_bringup'), 'maps' ,'icra2024.yaml'),
-         'robot_names_file': os.path.join(get_package_share_directory('driving_swarm_bringup'), 'params', 'robot_names_sim.yaml'),
-         'rosbag_topics_file': os.path.join(get_package_share_directory('trajectory_follower'), 'params', 'rosbag_topics_reactive.yaml'),
-         'qos_override_file': os.path.join(get_package_share_directory('experiment_measurement'), 'params', 'qos_override.yaml')
-    }
-
-    robots_file = args['robot_names_file']
-    with open(robots_file, 'r') as stream:
-        robots = yaml.safe_load(stream)
-    n_robots = len(robots)
-
-    poses = generate_random_poses(n_robots)
-
-    tmp_poses_file = tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.yaml')
-    yaml.dump(poses, tmp_poses_file, default_flow_style=True)
-    tmp_poses_file.close()
-
-    args['poses_file'] = tmp_poses_file.name
-
-    multi_robot_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('driving_swarm_bringup'), 'launch', 'multi_robot.launch.py')),
-        launch_arguments=args.items())
-
-    ld = LaunchDescription()
-    ld.add_action(multi_robot_launch)
-    ld.add_action(DeclareLaunchArgument('data_file', default_value=EnvironmentVariable('DATA_FILE', default_value='data.csv.gz')))
-    ld.add_action(OpaqueFunction(function=controller_spawning))
-    return ld
