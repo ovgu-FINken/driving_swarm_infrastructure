a10b8edc0098b9292a8fac5e9bd665e0fe134e66
Michael Dubé <michael.dube@ovgu.de>
2025-10-22 12:51:00 +0200

    Added first version of math for sunburst method.

--- src/msx/msx/sunburst_robot_calc.py
+++ src/msx/msx/sunburst_robot_calc.py
@@ -7,7 +7,7 @@ from visualization_msgs.msg import Marker, MarkerArray
 from sensor_msgs.msg import LaserScan
 from driving_swarm_utils.utils import detect_tb_from_ranges
 import numpy as np
-import time
+import time, math
 
 class SunburstRobotCalc(DrivingSwarmNode):
     def __init__(self, name: str) -> None:
@@ -39,11 +39,81 @@ class SunburstRobotCalc(DrivingSwarmNode):
         self.pub_error = self.create_publisher(Float64MultiArray, "/sunburstRobotCalc/waldoPosition", 100)
 
 
-    def do_math(self):
-        # self.get_logger().info('do_math')
-        if len(self.lidar_data) > 0:
-            self.get_logger().info(f"Robot Positions: {self.lidar_data}")
+    def calc_angle_error(self, alpha, sigmas, thetas):
+        sum = 0
+        for idx in range(len(sigmas)):
+            sum += (alpha + sigmas[idx] - thetas[idx])**2
             pass
+        return sum
+
+
+    def calc_distance_error(self, scale, Ls, ls):
+        sum = 0
+        for idx in range(len(ls)):
+            sum += (scale*ls[idx] - Ls[idx])**2
+            pass
+        return sum
+
+
+    def do_math(self):
+        # Okay at this point I have self.skyview_distances, self.skyview_angles, self.lidar_data
+        # Assume N robots
+        # self.skyview_angles[robot 0-(N-1)][0] = the angle between the x-axis of the indexed robot and the closest robot (this should be 0 as the closest robot determines the x-axis)
+        # self.skyview_angles[robot 0-(N-1)][1-(N-2)] = the angle between the x-axis of the indexed robot and the next N-2 robots (in clockwise order from the x-axis)
+
+        # self.skyview_distances[robot 0-(N-1)][0] = the distance to the closest robot (which forms the x-axis)
+        # self.skyview_distances[robot 0-(N-1)][1-(N-2)] = the distance to the next N-2 robots (sorted according to the angles in self.skyview_angles)
+
+        # self.lidar_data[robot 0-(N-2)][...] = a list of np arrays each with the x and y positions (respectively) of a robot relative to the position of the robot
+
+        self.lidar_angles = []
+        self.lidar_distances = []
+
+        for loc in self.lidar_data:
+            angle = math.atan2(loc[1], loc[0])
+            if angle < 0:
+                angle += 2 * math.pi
+                pass
+            self.lidar_angles.append(angle)
+            self.lidar_distances.append(math.sqrt(loc[0]**2 + loc[1]**2))
+            pass
+
+        # For the data from each of the N robots
+        min_scales = []
+        min_angles = []
+        for rbt_idx in range(len(self.skyview_angles)):
+            divisor = 1
+            sum = 0
+            for other_idx in range(len(self.skyview_angles[rbt_idx])):
+                sum -= 2*self.lidar_angles[other_idx]
+                sum += 2*self.skyview_angles[rbt_idx][other_idx]
+                divisor += 2
+                pass
+            angle = sum/divisor
+            min_angles.append(angle)
+
+            divisor = 0
+            sum = 0
+            for other_idx in range(len(self.skyview_distances[rbt_idx])):
+                sum += 2*self.skyview_distances[rbt_idx][other_idx]*self.lidar_distances[other_idx]
+                divisor += 2*self.skyview_distances[rbt_idx][other_idx]**2
+                pass
+            scale = sum/divisor
+            min_scales.append(scale)
+            pass
+
+        angle_errors = []
+        scale_errors = []
+        for rbt_idx in range(len(self.skyview_angles)):
+            angle_errors.append(self.calc_angle_error(min_angles[rbt_idx], self.lidar_angles, self.skyview_angles[rbt_idx]))
+            scale_errors.append(self.calc_distance_error(min_scales[rbt_idx], self.lidar_distances, self.skyview_distances[rbt_idx]))
+            pass
+
+        angle_idx = angle_errors.index(min(angle_errors))
+        scale_idx = scale_errors.index(min(scale_errors))
+
+        self.get_logger().info(f"I AM {angle_idx} I AM {scale_idx}")
+
         # self.get_logger().info(self.skyview_distances)
         # self.get_logger().info(self.skyview_angles)
 
@@ -54,15 +124,35 @@ class SunburstRobotCalc(DrivingSwarmNode):
 
         pass
 
+
     def calc_timer(self):
-        self.do_math()
+        # if len(self.lidar_data) > 0:
+        #     self.get_logger().info(f"Robot Positions: {self.lidar_data}")
+        #     pass
+
+        # To ensure the math won't have a division by zero error!
+        if self.skyview_angles is not None and self.skyview_distances is not None:
+            if len(self.lidar_data) > 1 and len(self.skyview_angles) > 0 and len(self.skyview_distances) > 0:
+                self.do_math()
+            else:
+                self.get_logger().info(f"DATA MISSING")
+            pass
         pass
 
     def laser_callback(self, msg):
         r = msg.ranges
         r = [x if x > msg.range_min and x < msg.range_max else 10.0 for x in r]
-        self.lidar_data = detect_tb_from_ranges(r, 0.0, 0.0, 0.0, msg.angle_min, msg.angle_increment, cluster_size_threshold=20)
+        self.lidar_data = detect_tb_from_ranges(r, 0.0, 0.0, 0.0, msg.angle_min, msg.angle_increment, cluster_range_threshold=3.5, cluster_size_threshold=20)
+        # The output of the positions is relative to the current position of the laser scanner
+        # Using the x-axis of the robot (aka the first value from the msg.ranges)
+
+        # Params
+        # Cluster linkage threshold -> How far the cluster has to be from another cluster (closeness of points to be considered in the same cluster) this happens first
+        # Cluster range threshold -> Points larger than this value are omitted from clusters
+        # Cluster size threshold -> any cluster with less than this number of points CAN be still consdiered a turtle bot
         # self.get_logger().info(f"Robots: {self.lidar_data}")
+        # Change range threshold to 3.5
+        # Look at what the RoLo guys did ...
 
         marker_array = MarkerArray()
 
