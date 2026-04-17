abe84f89e8d531d6b5cedcd1d6d49ad8b3a234d5
Michael Dubé <michael.dube@ovgu.de>
2026-04-17 15:29:09 +0200

    Updated detect_tb_from_ranges to remove many of the edge cases.

--- src/driving_swarm_utils/driving_swarm_utils/utils.py
+++ src/driving_swarm_utils/driving_swarm_utils/utils.py
@@ -4,112 +4,118 @@ import math
 import numpy as np
 
 
+'''
+Detect any TurtleBots using the LiDAR ranges.
+
+Cluster linkage threshold -> How close two successive points must be to be considered in the same cluster
+Cluster range threshold -> Points larger than this value are omitted from clusters
+Cluster size threshold -> Any cluster with less than this number of points COULD be considered a TurtleBot
+'''
 def detect_tb_from_ranges(ranges,
                           px=0.0,
                           py=0.0,
                           pt=0.0,
                           angle_min=0.0,
                           angle_increment=1.0,
+                          minimum_range=0.12,
+                          object_radius=0.035,
                           cluster_linkage_threshold=0.15,
+                          max_invalids_within_cluster=2,
                           cluster_range_threshold=1.5,
-                          cluster_size_threshold=30):
-    # detect turtlebot in scan ranges
-    # cluster_label = np.zeros_like(ranges, dtype=np.int32)
+                          cluster_size_threshold=20):
+    assert len(ranges)>0
 
-    # compute single-linkage clusters, where points are connected if they are closer than cluster_threshold
+    # Compute single-linkage clusters, where points are connected if they are closer than cluster_threshold
     last_range = None
+    invalid_count = 0
     clusters = []
     cur_cluster = []
-    for idx, r in enumerate(ranges):
-        if last_range is None:
-            last_range = r
-        # include invalid measurements (more important for the real robots)
-        # TODO: Think about how this might impact the centre of a cluster (perhaps switch this to None or NAN)
-        # THING = float('nan')
-        if r > 9.0:
+    for idx, cur_range in enumerate(ranges):
+        # Record the most recently seen range
+        if last_range is None and cur_range < cluster_range_threshold:
+            last_range = cur_range
+
+        # Append any points at or beyond the sensor's maximum range to the current cluster (erroneous range or range from gap in robot)
+        if cur_range >= cluster_range_threshold: # Invalid
+            if invalid_count > max_invalids_within_cluster:
+                # Too many invalids, force a cluster split
+                clusters.append(cur_cluster)
+                cur_cluster = [idx]
+                invalid_count = 0
+            else:
+                cur_cluster.append(idx)
+                invalid_count += 1
+            continue # Skip the threshold check between two points and add it to the cluster
+
+        assert(last_range is not None)
+        # Two successive valid ranges satisfy the cluster_linkage_threshold * (the number of invalids between them + 1)
+        if np.abs(cur_range - last_range) < (cluster_linkage_threshold * (invalid_count + 1)):
             cur_cluster.append(idx)
-            continue
-        if np.abs(r - last_range) < cluster_linkage_threshold:
-            cur_cluster.append(idx)
-        else:  # if the distance is greater then start a new cluster
+        # If the distance is greater than cluster_linkage_threshold then start a new cluster
+        else:
             clusters.append(cur_cluster)
             cur_cluster = [idx]
-        last_range = r
+        invalid_count = 0 # Reset the invalid count as we have a valid range if we got here
+        last_range = cur_range
+
+    # Append the final cluster
     clusters.append(cur_cluster)
 
-    # TODO: Offset from where the bots are detected and the actual robot is (due to the points being closer than the centre)
+    # If the last cluster is close to the first cluster, connect them
+    invalids_between = 0
+    first_valid = -1
+    for idx in range(len(clusters[0])):
+        if ranges[clusters[0][idx]] < cluster_range_threshold:  # Valid
+            first_valid = idx
+            break  # Found
+        else:
+            invalids_between += 1
 
-    # TODO: The robots have to be closer than the further away points to either side (error)
+    last_valid = len(clusters[-1])
+    for idx in range(len(clusters[-1]) - 1, -1, -1):
+        if ranges[clusters[-1][idx]] < cluster_range_threshold:  # Valid
+            last_valid = idx
+            break  # Found
+        else:
+            invalids_between += 1
 
-    # TODO: Add a edge case for invalid points appearing in the cluster
-    # if r[-1] and r[0] are close, connect the clusters
-    if np.abs(ranges[clusters[0][0]] - ranges[clusters[-1][-1]]) < cluster_linkage_threshold:
+    # An entire cluster is all invalid or the first and last cluster are the same, skip connecting
+    if len(clusters) < 2 or first_valid == -1 or last_valid == len(clusters[-1]):
+        pass
+    elif (invalids_between <= max_invalids_within_cluster
+          and np.abs(ranges[clusters[0][first_valid]] - ranges[clusters[-1][last_valid]]) <
+          (cluster_linkage_threshold * (invalids_between + 1))):
         clusters[0] = clusters[-1] + clusters[0]
         clusters = clusters[:-1]
-        pass
 
-    # compute cluster sizes
-    # cluster_sizes = np.bincount(cluster_label)
-    # cluster_sizes = [int(cluster_sizes[i]) for i in range(len(cluster_label)) if cluster_sizes[i] > 0]
-    #
-    # print(cluster_sizes)
+    # Remove all invalid points from each cluster
+    clusters = [[i for i in cluster if ranges[i] < cluster_range_threshold] for cluster in clusters]
+    # Remove entirely invalid clusters
+    clusters = [c for c in clusters if len(c) > 0]
 
-    # We want to clean the clusters so that invalid points at the start or end are removed from the clusters
-    invalid_cutoff = 1.5
-    for clust_idx in range(len(clusters)):  # For each cluster
-        # There are some invalid contained here
-        if any(ranges[clusters[clust_idx][i]] > invalid_cutoff for i in range(len(clusters[clust_idx]))):
-            # The entire cluster is invalid -> remove it
-            if all(ranges[clusters[clust_idx][i]] > invalid_cutoff for i in range(len(clusters[clust_idx]))):
-                clusters[clust_idx] = None
-                continue
-
-            # Some invalid, but not all
-            # Let's check the start of the cluster
-            first_valid = -1 # This must happen because there are some invalid, but not all
-            for idx in range(len(clusters[clust_idx])): # For each idx in the cluster
-                if ranges[clusters[clust_idx][idx]] <= invalid_cutoff: # Valid
-                    first_valid = idx
-                    break # Found
-                pass
-            clusters[clust_idx] = clusters[clust_idx][first_valid:]
-
-            # Let's check the end of the cluster
-            first_valid = len(clusters[clust_idx])
-            for idx in range(len(clusters[clust_idx]) - 1, 0, -1): # For each idx in the cluster
-                if ranges[clusters[clust_idx][idx]] <= invalid_cutoff: # Valid
-                    first_valid = idx
-                    break # Found
-                pass
-            clusters[clust_idx] = clusters[clust_idx][:first_valid + 1]
-            pass
-        else: # All valid -> check the next cluster
-            continue
-
-    clusters = [c for c in clusters if c is not None]
-
-    # for each cluster, compute the mean angle and range
-    # cluster_center_index = np.zeros(len(clusters), dtype=np.int32)
+    # For each cluster, compute the mean angle and range of the valid points in the cluster (the positions of the TurtleBots)
     tb_center_points = []
-    # this stores the detected positions of the turtlebots
     for clust_idx in range(len(clusters)):
-        # Ignore clusters that are too small
+        # Ignore clusters that are too big
         if len(clusters[clust_idx]) > cluster_size_threshold:
             continue
-        # Get the lowest(first) index of the cluster
-        first_idx = clusters[clust_idx][0]
-        # TODO: solve the edge case of the cluster wrapping around 0 degrees (min_idx should be first index)
 
-        # cluster_center_index[clust_idx] = (first_idx + int(len(clusters[clust_idx]) / 2)) % len(ranges)
         cluster_ranges = [ranges[idx] for idx in clusters[clust_idx]]
         cr = np.median(cluster_ranges)
-        # TODO: EDGE CASE, this might be invalid.
-        if cr > cluster_range_threshold:
+
+        # The median range should be less than the cluster_range_threshold
+        assert cr < cluster_range_threshold
+
+        # The median range should be more than the minimum_range of the LiDAR sensor
+        if cr < minimum_range:
             continue
-        # TODO: Modify this to be more smart
-        if len(clusters[clust_idx]) > 0.0695 * 360 / (2 * math.pi * (cr-0.05)) + 3:
+
+        # The actual centre of the robot is a radius from the median range cr
+        cr = cr + object_radius
+
+        if len(clusters[clust_idx]) > 2 * object_radius * 180 / (math.pi * (cr-0.05)) + 3:
             continue
-        if len(clusters[clust_idx]) < 0.0695 * 360 / (2 * math.pi * cr) - 3:
+        if len(clusters[clust_idx]) < 2 * object_radius * 180 / (math.pi * cr) - 3:
             continue
         if len(clusters[clust_idx]) < 3:
             continue
@@ -120,7 +126,6 @@ def detect_tb_from_ranges(ranges,
         if mean_distance_to_center > 0.05:
             continue
         tb_center_points.append(cluster_center)
-        # TODO: Edge case where there is an invalid point in the middle of the turtle bot cluster
     return tb_center_points
 
 
